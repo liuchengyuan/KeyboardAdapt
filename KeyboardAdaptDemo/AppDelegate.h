1bcf953da14a02d7d52fc391c98b672e915a63f5
CY L <liuchengyuan@liuchengyuandemacbook-pro.local>
2016-05-12 17:56:42 +0800

    Initial Commit

---
+++ KeyboardAdaptDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyboardAdaptDemo
+//
+//  Created by LCY on 16/5/12.
+//  Copyright © 2016年 LCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyboardAdaptDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyboardAdaptDemo
+//
+//  Created by LCY on 16/5/12.
+//  Copyright © 2016年 LCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
